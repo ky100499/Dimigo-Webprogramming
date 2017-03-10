836160f1ece132f4d1068904624a196344e2f63d
ky100499 <ky100499@naver.com>
2017-03-10 10:57:34 +0900

    Init
    Initial Commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$date = new DateTime('now', new DateTimeZone('Asia/Seoul'));
+$today = isset($_GET['date']) ? $_GET['date'] : $date->format('Ymd');
+
+$target = './' . $today;
+if (!file_exists($target)) {
+    mkdir($target);
+
+    $base_html = "<!DOCTYPE html>\n<html>\n<head>\n    <title>" . $today . "</title>\n    <meta charset=\"utf-8\">\n</head>\n<body>\n</body>\n</html>";
+
+    $fp = fopen($target . "/index.php", "w");
+    fwrite($fp, $base_html);
+    fclose($fp);
+}
+
+require('./' . $today . '/index.php');
+?>
