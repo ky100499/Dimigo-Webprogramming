5974c0805aa612aa9f905a06dde54896693543ce
ky100499 <ky100499@naver.com>
2017-03-23 10:27:17 +0900

    20170323_temp

---
+++ projects/20170323/20170323_3624_이광연.php
@@ -0,0 +1,22 @@
+<!--
+    Auto-Generated file
+    leekwangyeon 20170323
+-->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>20170323</title>
+    <meta charset="utf-8">
+</head>
+<body>
+    <p style="border-bottom: 3px solid green">3624 이광연</p>
+
+    <a href="http://dimigo.in" target="h3624a">디미고인</a>
+    <a href="http://dimigo.hs.kr" target="h3624b">디미고</a>
+
+    <hr>
+
+    <iframe name="h3624a" src="./h3624a.html" width="800" height="400"></iframe>
+    <iframe name="h3624b" src="./h3624b.html" width="800" height="400"></iframe>
+</body>
+</html>
