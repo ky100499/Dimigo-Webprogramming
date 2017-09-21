a1b2c5e25e395a317b0a07e8d4ada77c424c06ab
ky100499 <ky100499@naver.com>
2017-09-21 09:36:45 +0900

    20170921

---
+++ projects/20170921/20170921_3624.php
@@ -0,0 +1,27 @@
+<?php
+$dbinfo = json_decode(file_get_contents("dbinfo.json"));
+$conn = mysqli_connect(
+    $dbinfo->host,
+    $dbinfo->user,
+    $dbinfo->password,
+    $dbinfo->dbname
+) or die("Cannot connect database");
+
+$query = "
+    CREATE TABLE students (
+        id int(6) unsigned AUTO_INCREMENT PRIMARY KEY,
+        snum varchar(5) NOT NULL,
+        sname varchar(30) NOT NULL,
+        pw varchar(64) NOT NULL,
+        dept varchar(20) NOT NULL,
+        gender varchar(4) NOT NULL,
+        reg_date TIMESTAMP
+    )
+";
+if (mysqli_query($conn, $query)) {
+    echo "Table student created";
+} else {
+    echo "Error while creating table: " . $conn->error;
+}
+$conn->close();
+?>
